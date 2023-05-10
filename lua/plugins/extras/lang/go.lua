f1ecb93a19fb42fa5f9435773f89b2b4220b03a1
wortox <44939520+wortoxs@users.noreply.github.com>
2023-05-10 20:37:35 +0800

    feat(debug): add debug support for go

--- lua/plugins/extras/lang/go.lua
+++ lua/plugins/extras/lang/go.lua
@@ -20,14 +20,15 @@ return {
   {
     "ray-x/go.nvim",
     dependencies = { -- optional packages
-      --   "ray-x/guihua.lua",
+      "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-      require("go").setup()
+      require("go").setup({ dap_debug_gui = false, })
     end,
     event = { "CmdlineEnter" },
+    lazy = true,
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
