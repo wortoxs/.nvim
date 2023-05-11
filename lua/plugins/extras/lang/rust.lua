2659edfc8742602eb43aa66bfff1ed1efab65003
wortox <44939520+wortoxs@users.noreply.github.com>
2023-05-11 10:42:13 +0800

    fix(dap): check if codelldb is installed before using

--- lua/plugins/extras/lang/rust.lua
+++ lua/plugins/extras/lang/rust.lua
@@ -48,23 +48,24 @@ return {
           local codelldb_path = ""
           local liblldb_path = ""
           local this_os = vim.loop.os_uname().sysname
-
           local mason_registry = require("mason-registry")
-          local codelldb = mason_registry.get_package("codelldb") -- note that this will error if you provide a non-existent package name
-          local codelldb_install_path = codelldb:get_install_path() -- returns a string like "/home/user/.local/share/nvim/mason/packages/codelldb"
-          -- The path in windows is different
-          if this_os:find("Windows") then
-            codelldb_path = codelldb_install_path .. "\\extension\\adapter\\codelldb.exe"
-            liblldb_path = codelldb_install_path .. "\\extension\\lldb\\bin\\liblldb.dll"
-          else
-            -- The liblldb extension is .so for linux and .dylib for macOS
-            codelldb_path = codelldb_install_path .. "/extension" .. "/adapter/codelldb"
-            liblldb_path = codelldb_install_path
-              .. "/extension"
-              .. "/lldb/lib/liblldb"
-              .. (this_os == "Linux" and ".so" or ".dylib")
-          end
 
+          if mason_registry.is_installed("codelldb") then
+            local codelldb = mason_registry.get_package("codelldb") -- note that this will error if you provide a non-existent package name
+            local codelldb_install_path = codelldb:get_install_path() -- returns a string like "/home/user/.local/share/nvim/mason/packages/codelldb"
+            -- The path in windows is different
+            if this_os:find("Windows") then
+              codelldb_path = codelldb_install_path .. "\\extension\\adapter\\codelldb.exe"
+              liblldb_path = codelldb_install_path .. "\\extension\\lldb\\bin\\liblldb.dll"
+            else
+              -- The liblldb extension is .so for linux and .dylib for macOS
+              codelldb_path = codelldb_install_path .. "/extension" .. "/adapter/codelldb"
+              liblldb_path = codelldb_install_path
+                .. "/extension"
+                .. "/lldb/lib/liblldb"
+                .. (this_os == "Linux" and ".so" or ".dylib")
+            end
+          end
           local rust_tool_opts = vim.tbl_deep_extend("force", opts, {
             dap = {
               adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
