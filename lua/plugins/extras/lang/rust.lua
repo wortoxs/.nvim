38942de8fd48e5f1124ac8d7541d3e5367873882
wortox <44939520+wortoxs@users.noreply.github.com>
2023-07-19 13:52:31 +0800

    refactor: change the lsp plugin to the official code
    rust 的调试工具 codelldb 官方给的配置没有适配
    windows，因此做了一些修改。虽然调试功能是能用了，但是在调试退出的时仍然会有警告(rust 和 go 都有这个问题)

--- lua/plugins/extras/lang/rust.lua
+++ lua/plugins/extras/lang/rust.lua
@@ -1,80 +1,155 @@
--- Update this path
-
 return {
-  -- add rust to treesitter
+
+  -- Extend auto completion
   {
-    "nvim-treesitter/nvim-treesitter",
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "Saecki/crates.nvim",
+        event = { "BufRead Cargo.toml" },
+        config = true,
+      },
+    },
+    ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      table.insert(opts.ensure_installed, "rust")
-      table.insert(opts.ensure_installed, "toml")
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "crates" },
+      }))
     end,
   },
 
-  -- correctly setup lspconfig for Rust 🦀
+  -- Add Rust & related to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "ron", "rust", "toml" })
+      end
+    end,
+  },
+
+  -- Ensure Rust debugger is installed
+  {
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "codelldb" })
+      end
+    end,
+  },
+
+  {
+    "simrat39/rust-tools.nvim",
+    lazy = true,
+    opts = function()
+      local ok, mason_registry = pcall(require, "mason-registry")
+      local adapter ---@type any
+      if ok then
+        -- rust tools configuration for debugging support
+        local codelldb = mason_registry.get_package("codelldb")
+        local extension_path = codelldb:get_install_path()
+          .. (vim.fn.has("win32") == 1 and "\\extension\\" or "/extension/")
+        local codelldb_path = extension_path
+          .. (vim.fn.has("win32") == 1 and "adapter\\codelldb.exe" or "adapter/codelldb")
+        local liblldb_path = vim.fn.has("win32") == 1 and extension_path .. "lldb\\bin\\liblldb.dll"
+          or vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
+          or extension_path .. "lldb/lib/liblldb.so"
+        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+      end
+      return {
+        dap = {
+          adapter = adapter,
+        },
+        tools = {
+          on_initialized = function()
+            vim.cmd([[
+                  augroup RustLSP
+                    autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
+                    autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
+                    autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
+                  augroup END
+                ]])
+          end,
+        },
+      }
+    end,
+    config = function() end,
+  },
+
+  -- Correctly setup lspconfig for Rust 🚀
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "simrat39/rust-tools.nvim",
-    },
     opts = {
       servers = {
+        -- Ensure mason installs the server
         rust_analyzer = {
           mason = false,
+          keys = {
+            { "K", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+            { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+            { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
+          },
           settings = {
             ["rust-analyzer"] = {
-              imports = {
-                granularity = {
-                  group = "module",
-                },
-                prefix = "self",
-              },
               cargo = {
-                buildScripts = {
-                  enable = true,
-                },
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                runBuildScripts = true,
+              },
+              -- Add clippy lints for Rust.
+              checkOnSave = {
+                allFeatures = true,
+                command = "clippy",
+                extraArgs = { "--no-deps" },
               },
               procMacro = {
                 enable = true,
+                ignored = {
+                  ["async-trait"] = { "async_trait" },
+                  ["napi-derive"] = { "napi" },
+                  ["async-recursion"] = { "async_recursion" },
+                },
               },
-              checkOnSave = {
-                command = "clippy",
-              },
+            },
+          },
+        },
+        taplo = {
+          keys = {
+            {
+              "K",
+              function()
+                if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+                  require("crates").show_popup()
+                else
+                  vim.lsp.buf.hover()
+                end
+              end,
+              desc = "Show Crate Documentation",
             },
           },
         },
       },
       setup = {
         rust_analyzer = function(_, opts)
-          local codelldb_path = ""
-          local liblldb_path = ""
-          local this_os = vim.loop.os_uname().sysname
-          local mason_registry = require("mason-registry")
-
-          if mason_registry.is_installed("codelldb") then
-            local codelldb = mason_registry.get_package("codelldb") -- note that this will error if you provide a non-existent package name
-            local codelldb_install_path = codelldb:get_install_path() -- returns a string like "/home/user/.local/share/nvim/mason/packages/codelldb"
-            -- The path in windows is different
-            if this_os:find("Windows") then
-              codelldb_path = codelldb_install_path .. "\\extension\\adapter\\codelldb.exe"
-              liblldb_path = codelldb_install_path .. "\\extension\\lldb\\bin\\liblldb.dll"
-            else
-              -- The liblldb extension is .so for linux and .dylib for macOS
-              codelldb_path = codelldb_install_path .. "/extension" .. "/adapter/codelldb"
-              liblldb_path = codelldb_install_path
-                .. "/extension"
-                .. "/lldb/lib/liblldb"
-                .. (this_os == "Linux" and ".so" or ".dylib")
-            end
-          end
-          local rust_tool_opts = vim.tbl_deep_extend("force", opts, {
-            dap = {
-              adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-            },
-          })
-
-          require("rust-tools").setup(rust_tool_opts)
+          local rust_tools_opts = require("lazyvim.util").opts("rust-tools.nvim")
+          require("rust-tools").setup(vim.tbl_deep_extend("force", rust_tools_opts or {}, { server = opts }))
           return true
         end,
+      },
+    },
+  },
+
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "rouge8/neotest-rust",
+    },
+    opts = {
+      adapters = {
+        ["neotest-rust"] = {},
       },
     },
   },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,3 @@
 return {
   { import = "plugins.extras.lang.rust" },
-  { import = "plugins.extras.lang.go" },
-  { import = "plugins.extras.lang.clangd" },
 }
